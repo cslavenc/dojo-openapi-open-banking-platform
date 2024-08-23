095f7c84885a1748aed90335734b976e687d42d2
cslavenc <slavenc@hotmail.ch>
2024-08-23 09:53:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# dojo-openapi
+
+In this dojo, we take a look at OpenAPI and how contract-first principles help us create RESTful APIs in a more
+consistent and faster manner.
---
+++ java/open-banking-platform/src/test/java/dojo/openapi/open_banking_platform/OpenapiOpenBankingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dojo.openapi.open_banking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiOpenBankingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
