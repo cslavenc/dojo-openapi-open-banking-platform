6f4f0fdc2d6594ce111a7d515428a1e858f3f237
cslavenc <slavenc@hotmail.ch>
2024-08-24 10:30:00 +0200

    Add solution folder

---
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/OpenapiOpenBankingPlatformApplication.java
@@ -0,0 +1,13 @@
+package dojo.openapi.open_banking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiOpenBankingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiOpenBankingPlatformApplication.class, args);
+	}
+
+}
