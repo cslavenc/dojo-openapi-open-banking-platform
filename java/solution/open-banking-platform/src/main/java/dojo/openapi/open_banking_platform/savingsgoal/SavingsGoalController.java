e9d0a000aa997399848cfb4657b9cf9d1ed5da00
cslavenc <github.avey7@passmail.net>
2024-09-03 07:46:09 +0200

    Return default data

--- java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/savingsgoal/SavingsGoalController.java
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/savingsgoal/SavingsGoalController.java
@@ -2,7 +2,7 @@ package dojo.openapi.open_banking_platform.savingsgoal;
 
 import dojo.openapi.open_banking_platform.api.SavingsGoalsApi;
 import dojo.openapi.open_banking_platform.model.SavingsGoalDTO;
-import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +13,21 @@ public class SavingsGoalController implements SavingsGoalsApi {
 
     @Override
     public ResponseEntity<SavingsGoalDTO> getSavingsGoalDetails(UUID goalId) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(new SavingsGoalDTO(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Void> createSavingsGoal(SavingsGoalDTO savingsGoalDTO) {
-        throw new NotImplementedException("Method not implemented");
+    public ResponseEntity<SavingsGoalDTO> createSavingsGoal(SavingsGoalDTO savingsGoalDTO) {
+        return new ResponseEntity<>(savingsGoalDTO, HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<Void> updateSavingsGoal(UUID goalId, SavingsGoalDTO savingsGoalDTO) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> deleteSavingsGoal(UUID goalId) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/savingsgoal/SavingsGoalData.java
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/savingsgoal/SavingsGoalData.java
@@ -1,4 +1,27 @@
 package dojo.openapi.open_banking_platform.savingsgoal;
 
+import dojo.openapi.open_banking_platform.model.SavingsGoalDTO;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class SavingsGoalData {
+
+    private static final UUID goalId = UUID.randomUUID();
+    private static final String name = "Savings Goal";
+    private static final Double targetAmount = 25000.25;
+    private static final Double progress = 10100.50;
+    private static final LocalDate startDate = LocalDate.of(2020, 1, 1);
+    private static final LocalDate endDate = LocalDate.of(2020, 12, 31);
+
+
+    public static SavingsGoalDTO defaultSavingsGoal() {
+        return new SavingsGoalDTO()
+                .goalId(goalId)
+                .name(name)
+                .targetAmount(targetAmount)
+                .progress(progress)
+                .startDate(startDate)
+                .endDate(endDate);
+    }
 }
