ec4bda40dbf08259c4c780fc4b5c5af240422a24
cslavenc <github.avey7@passmail.net>
2024-09-03 07:46:00 +0200

    Update comment

--- java/exercise/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/savingsgoal/SavingsGoalData.java
+++ java/exercise/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/savingsgoal/SavingsGoalData.java
@@ -3,6 +3,7 @@ package dojo.openapi.open_banking_platform.savingsgoal;
 /**
  * TODO : feel free to use this class for static data to be returned to the UI
  * This project also supports lombok, so annotations such as @Builder or @Getter are supported!
+ * E.g. new SavingsGoalDTO().goalId(goalId) works as the DTOs are annotated with @Builder
  */
 public class SavingsGoalData {
 }
