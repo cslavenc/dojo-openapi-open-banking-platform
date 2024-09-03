35a6d165409f3ef1d529b031ac0dcec7dc8c2ac3
cslavenc <github.avey7@passmail.net>
2024-09-03 07:28:09 +0200

    Return default data

--- java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/budget/BudgetController.java
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/budget/BudgetController.java
@@ -3,6 +3,7 @@ package dojo.openapi.open_banking_platform.budget;
 import dojo.openapi.open_banking_platform.api.BudgetsApi;
 import dojo.openapi.open_banking_platform.model.BudgetDTO;
 import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +14,21 @@ public class BudgetController implements BudgetsApi {
     
     @Override
     public ResponseEntity<BudgetDTO> getBudgetDetails(UUID budgetId) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(BudgetData.defaultBudget(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Void> createBudget(BudgetDTO budgetDTO) {
-        throw new NotImplementedException("Method not implemented");
+    public ResponseEntity<BudgetDTO> createBudget(BudgetDTO budgetDTO) {
+        return new ResponseEntity<>(BudgetData.defaultBudget(), HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<Void> updateBudgetSettings(UUID budgetId, BudgetDTO budgetDTO) {
-        throw new NotImplementedException("Method not implemented");
+    public ResponseEntity<BudgetDTO> updateBudgetSettings(UUID budgetId, BudgetDTO budgetDTO) {
+        return new ResponseEntity<>(BudgetData.defaultBudget(), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> deleteBudget(UUID budgetId) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/budget/BudgetData.java
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/budget/BudgetData.java
@@ -1,4 +1,29 @@
 package dojo.openapi.open_banking_platform.budget;
 
+import dojo.openapi.open_banking_platform.model.BudgetDTO;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.UUID;
+
 public class BudgetData {
+
+    private static final UUID budgetId = UUID.randomUUID();
+    private static final String name = "Budget";
+    private static final LocalDate startDate = LocalDate.of(2019, 1, 1);
+    private static final LocalDate endDate = LocalDate.of(2019, 12, 31);
+    private static final Double savingsGoal = 8080.99;
+    private static final Double monthlyIncome = 7070.70;
+    private static final Map<String, Double> monthlyExpense = Map.of();
+
+    public static BudgetDTO defaultBudget() {
+        return new BudgetDTO()
+                .budgetId(budgetId)
+                .name(name)
+                .startDate(startDate)
+                .endDate(endDate)
+                .savingsGoal(savingsGoal)
+                .monthlyIncome(monthlyIncome)
+                .monthlyExpenses(monthlyExpense);
+    }
 }
