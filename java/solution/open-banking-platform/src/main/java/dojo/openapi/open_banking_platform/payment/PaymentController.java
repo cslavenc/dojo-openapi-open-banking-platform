f5e4d57a5353a82b5721dd3cccf648ceddb63707
cslavenc <github.avey7@passmail.net>
2024-09-03 07:39:27 +0200

    Return default data

--- java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/payment/PaymentController.java
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/payment/PaymentController.java
@@ -3,7 +3,7 @@ package dojo.openapi.open_banking_platform.payment;
 import dojo.openapi.open_banking_platform.api.PaymentsApi;
 import dojo.openapi.open_banking_platform.model.PaymentRequestDTO;
 import dojo.openapi.open_banking_platform.model.PaymentResponseDTO;
-import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,26 +15,26 @@ public class PaymentController implements PaymentsApi {
 
     @Override
     public ResponseEntity<PaymentResponseDTO> getPaymentDetails(UUID paymentId) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(PaymentData.defaultPaymentResponse(), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<List<PaymentResponseDTO>> getPaymentHistory() {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(PaymentData.defaultPaymentResponses(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Void> createPayment(PaymentRequestDTO paymentRequestDTO) {
-        throw new NotImplementedException("Method not implemented");
+    public ResponseEntity<PaymentResponseDTO> createPayment(PaymentRequestDTO paymentRequestDTO) {
+        return new ResponseEntity<>(PaymentData.defaultPaymentResponse(), HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<Void> updatePaymentStatus(UUID paymentId, PaymentRequestDTO paymentRequestDTO) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> cancelPayment(UUID paymentId) {
-        throw new NotImplementedException("Method not implemented");
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/payment/PaymentData.java
+++ java/solution/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/payment/PaymentData.java
@@ -1,4 +1,38 @@
 package dojo.openapi.open_banking_platform.payment;
 
+import dojo.openapi.open_banking_platform.model.PaymentRequestDTO;
+import dojo.openapi.open_banking_platform.model.PaymentResponseDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public class PaymentData {
+
+    private static final UUID paymentId = UUID.randomUUID();
+    private static final UUID transactionId = UUID.randomUUID();
+    private static final Double amount = 4000.40;
+    private static final String currency = "USD";
+    private static final String description = "instant payment";
+    private static final String recipient = "John Smith";
+    private static final String message = "Received";
+
+    public static PaymentRequestDTO defaultPaymentRequest() {
+        return new PaymentRequestDTO()
+                .paymentId(paymentId)
+                .amount(amount)
+                .currency(currency)
+                .description(description)
+                .recipient(recipient);
+    }
+
+    public static PaymentResponseDTO defaultPaymentResponse() {
+        return new PaymentResponseDTO()
+                .paymentId(paymentId)
+                .message(message)
+                .transactionId(transactionId);
+    }
+
+    public static List<PaymentResponseDTO> defaultPaymentResponses() {
+        return List.of(defaultPaymentResponse(), defaultPaymentResponse());
+    }
 }
