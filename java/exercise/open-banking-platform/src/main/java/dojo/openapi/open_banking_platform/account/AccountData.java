b9491ec84adfc31e2a4399923671a3bb6f894ab8
cslavenc <github.avey7@passmail.net>
2024-09-03 07:27:02 +0200

    Update comment

--- java/exercise/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/account/AccountData.java
+++ java/exercise/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/account/AccountData.java
@@ -3,6 +3,7 @@ package dojo.openapi.open_banking_platform.account;
 /**
  * TODO : feel free to use this class for static data to be returned to the UI
  * This project also supports lombok, so annotations such as @Builder or @Getter are supported!
+ * E.g. new AccountDetailsDTO().accountId(accountId) works as the DTOs are annotated with @Builder
  */
 public class AccountData {
 }
--- java/exercise/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/budget/BudgetData.java
+++ java/exercise/open-banking-platform/src/main/java/dojo/openapi/open_banking_platform/budget/BudgetData.java
@@ -3,6 +3,7 @@ package dojo.openapi.open_banking_platform.budget;
 /**
  * TODO : feel free to use this class for static data to be returned to the UI
  * This project also supports lombok, so annotations such as @Builder or @Getter are supported!
+ * E.g. new BudgetDTO().budgetId(accountId) works as the DTOs are annotated with @Builder
  */
 public class BudgetData {
 }
